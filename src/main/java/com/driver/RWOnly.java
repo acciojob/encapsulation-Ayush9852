58c201d26ae64f6b2f8f3953002c72236cd0c32b
Ayush9852 <71625438+Ayush9852@users.noreply.github.com>
2023-10-03 00:40:16 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    //Getter for read only access
+    public String getName() {
+        return name;
+    }
+    //Setter for write only access
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
